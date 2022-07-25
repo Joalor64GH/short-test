b906cac9ddbc0d750bd381e30f010580d4a72fb4
Joalor64 <rhgarcon@gmail.com>
2022-07-24 20:50:09 -0400

    zert6tggthwe

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -106,7 +106,7 @@ class TitleState extends MusicBeatState
 }
 		#if (polymod)
 		var loadedMods = "";
-		for (modData in modMetadata)
+		var (modData in modMetadata)
 			loadedMods += modData.title + "";
 
 		var modText = new FlxText(5, 5, 0, "", 16);
